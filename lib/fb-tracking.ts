c0b87923c053f47b83fdf3ed28a7aed00ff199c5
Keyner <contactoktweb@gmail.com>
2026-07-24 17:08:22 -0500

    cambio de pixel

--- lib/fb-tracking.ts
+++ lib/fb-tracking.ts
@@ -1,4 +1,4 @@
-export const FB_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || '1605719891129628';
+export const FB_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || '1346952427503441';
 const TEST_EVENT_CODE = process.env.NEXT_PUBLIC_META_TEST_EVENT_CODE;
 
 export const getCookie = (name: string): string | undefined => {
